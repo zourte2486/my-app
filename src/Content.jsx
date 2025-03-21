37e4ef164a29c829593f17769acd44fb907195fb
zourte2486 <zourte2486@gmail.com>
2025-03-21 00:32:16 +0000

    Refactor

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,9 +4,20 @@
 const Content = () => {
   const handleClick = () => {
     alert("You clicked me!");
+    
+  }
+  const handleClicks = (event) => {
+    console.log(event.type);
   }
 
-  return <button onClick={handleClick}>Click me</button>;
+
+
+  return <>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(event) => handleClicks(event)}>Click me</button>
+  </>
+
+  
 };
 
 export default Content;
